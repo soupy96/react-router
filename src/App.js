74cb78866d743eab33fddedd25642080d51fdef7
Michael Campbell <michael.campbell14@gmail.com>
2022-10-24 16:17:56 -0400

    first using params

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -15,9 +16,16 @@ function App() {
         <Route path='/products'>
           <Products />
         </Route>
+        <Route path='/product-detail/:productId'>
+          <ProductDetail />
+        </Route>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/<any value> => Product Detail Component
